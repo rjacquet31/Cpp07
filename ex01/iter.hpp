49b0537f507b7fedf48ab662818b09bc0afa22ce
Rjacquet <rafael.jacquet.31@gmail.com>
2026-03-03 14:30:07 +0100

    finit

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,6 +1,15 @@
+#include <cstddef>
+
 template <typename T>
 void iter(T *array, size_t length, void (*f)(const T &))
 {
     for (size_t i = 0; i < length; i++)
+        f(array[i]);
+}
+
+template <typename T, typename Func>
+void iter(T* array, std::size_t length, Func f)
+{
+    for (std::size_t i = 0; i < length; ++i)
         f(array[i]);
 }
